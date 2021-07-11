2bd3bdec8f7f960be24baf203550fb1f4d766225
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-07-11 22:03:22 +0200

    [neovim] Allow packer to update itself

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -1,7 +1,12 @@
-local install_path = vim.fn.stdpath('data')..'/site/pack/plugins/start/packer.nvim'
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   print('Packer doesn\'t exist at: ' .. install_path .. '. Downloading...')
   vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
   vim.api.nvim_command('packadd packer.nvim')
   print('Packer downloaded!')
 end
+
+return require('packer').startup(function()
+  -- Allow packer to update itself
+  use 'https://github.com/wbthomason/packer.nvim'
+end)
