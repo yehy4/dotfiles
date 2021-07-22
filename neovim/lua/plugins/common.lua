3b4929e2e579feeb932394219c7e521c5cd70fa3
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-07-22 23:29:15 +0200

    [neovim] Display relative file path in status line

--- neovim/lua/plugins/common.lua
+++ neovim/lua/plugins/common.lua
@@ -14,7 +14,15 @@ packer.use( {
   'https://github.com/hoob3rt/lualine.nvim',
   config = function ()
     require('lualine').setup({
-      options = { theme = 'material', icons_enabled = false }
+      options = { theme = 'material', icons_enabled = false },
+      sections = {
+        -- Display relative filepath in status line
+        lualine_c = {{'filename', path = 1}}
+      },
+      inactive_sections = {
+        -- Display relative filepath in status line
+        lualine_c = {{'filename', path = 1}}
+      }
     })
   end
 })
