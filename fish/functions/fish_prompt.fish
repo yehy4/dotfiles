8871cd9b8b80509fda90619176c092e0b3396102
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-07-24 22:29:01 +0200

    [fish] Display Git status in prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,6 +3,8 @@ function fish_prompt
   echo -n (set_color bryellow) $USER@(prompt_hostname)
   echo -n (set_color brgreen) $PWD
 
+  set --global __fish_git_prompt_show_informative_status 1
+
   set_color brcyan
   fish_git_prompt
   fish_hg_prompt
