3cb194a0965b5b5daa428603c1b6b5c4e0a6f571
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-07-24 02:11:50 +0200

    [neovim] Add keybinding to perform OSC52-based yanking

--- neovim/lua/plugins/yank.lua
+++ neovim/lua/plugins/yank.lua
@@ -1,2 +1,8 @@
--- Enable OSC 52 yanking
-require('packer').use('https://github.com/ojroques/vim-oscyank')
+-- Enable OSC52 yanking
+require('packer').use({
+  'https://github.com/ojroques/vim-oscyank',
+  config = function ()
+    -- Use <leader>y to perform OSC52 yanking when in visual mode
+    vim.api.nvim_set_keymap('v', '<leader>y', ':OSCYank<cr>', {noremap = true})
+  end
+})
