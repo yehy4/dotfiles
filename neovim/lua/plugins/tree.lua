fda9d222153568632259390258ce94082fba2e6c
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-07-27 22:34:59 +0200

    [neovim] Disable opening directory tree automatically on launch

--- neovim/lua/plugins/tree.lua
+++ neovim/lua/plugins/tree.lua
@@ -4,12 +4,10 @@ require('packer').use({
   config = function ()
     -- Show indentation marks for open folders
     vim.g.nvim_tree_indent_markers = 1
-    -- Open tree when launching nvim without arugments or in a directory
-    vim.g.nvim_tree_auto_open = 1
     -- Enable creating more than 2 splits without asking where a new split
     -- should be created
     vim.g.nvim_tree_disable_window_picker = 1
-    
+
     -- Use <leader>t to toggle
     vim.api.nvim_set_keymap('n', '<leader>t', ':NvimTreeToggle<cr>', {noremap = true})
   end
