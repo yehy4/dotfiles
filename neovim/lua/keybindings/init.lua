57c66be67a6d65d9960291aef498550011c3c4df
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-07-27 22:39:24 +0200

    [neovim] Open results window automatically after searching using keybinding

--- neovim/lua/keybindings/init.lua
+++ neovim/lua/keybindings/init.lua
@@ -1,2 +1,2 @@
 -- Use <leader>g to grep for an expression. This would usually be follow by :lw
-vim.api.nvim_set_keymap('n', '<leader>g', ':silent lgrep! ', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>g', ':silent lgrep!  | :lw<S-Left><S-Left><Left>', {noremap = true})
