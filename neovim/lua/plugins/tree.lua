2ca7e7336f585627ac38915c787406129b700f39
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-07-30 23:53:27 +0200

    [neovim] Lazy load nvim-tree plugin

--- neovim/lua/plugins/tree.lua
+++ neovim/lua/plugins/tree.lua
@@ -1,14 +1,16 @@
 -- Use a better file explorer than the built-in netrw
 require('packer').use({
   'https://github.com/kyazdani42/nvim-tree.lua',
+  opt = true, cmd = {'NvimTreeToggle', 'NvimTreeOpen'},
+  setup = function ()
+    -- Use <leader>t to toggle
+    vim.api.nvim_set_keymap('n', '<leader>t', ':NvimTreeToggle<cr>', {noremap = true})
+    end,
   config = function ()
     -- Show indentation marks for open folders
     vim.g.nvim_tree_indent_markers = 1
     -- Enable creating more than 2 splits without asking where a new split
     -- should be created
     vim.g.nvim_tree_disable_window_picker = 1
-
-    -- Use <leader>t to toggle
-    vim.api.nvim_set_keymap('n', '<leader>t', ':NvimTreeToggle<cr>', {noremap = true})
-  end
+    end
 })
