96da5d79142c708ff2b5003a7f588800d1067438
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-07-31 15:07:21 +0200

    [neovim] Add toggles for directory tree icons

--- neovim/lua/plugins/tree.lua
+++ neovim/lua/plugins/tree.lua
@@ -12,5 +12,12 @@ require('packer').use({
     -- Enable creating more than 2 splits without asking where a new split
     -- should be created
     vim.g.nvim_tree_disable_window_picker = 1
+
+    vim.g.nvim_tree_show_icons = {
+      git = 1,
+      folders = 1,
+      files = 1,
+      folder_arrows = 0
+    }
     end
 })
