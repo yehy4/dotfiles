d390bf016dd267659c01130404a3cfd5c9155a98
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-07-31 16:18:22 +0200

    [fish] Add Git information toggle using environment variable

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -4,9 +4,11 @@ function fish_prompt
   set --global fish_prompt_pwd_dir_length 0  # Disable path shortening
   echo -n (set_color brgreen) (prompt_pwd)
 
-  set_color brcyan
-  set --global __fish_git_prompt_show_informative_status 1
-  fish_git_prompt
+  if not set --query GIT_BASED_WORKFLOW ; or test $GIT_BASED_WORKFLOW != 0
+    set_color brcyan
+    set --global __fish_git_prompt_show_informative_status 1
+    fish_git_prompt
+  end
 
   set_color normal
   printf '\f\r--> '
