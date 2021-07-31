839ecefc22862d6ecbb7a1ccc5d336e1dd323b61
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-08-01 00:47:15 +0200

    [fish] Change prompt colors

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,15 +1,14 @@
 function fish_prompt
-  echo -n (set_color bryellow)$USER@(prompt_hostname)
+  echo -n (set_color magenta)$USER@(prompt_hostname)(set_color normal)
 
   set --global fish_prompt_pwd_dir_length 0  # Disable path shortening
-  echo -n (set_color brgreen) (prompt_pwd)
+  echo -n (set_color yellow) (prompt_pwd)(set_color normal)
 
   if not set --query GIT_BASED_WORKFLOW ; or test $GIT_BASED_WORKFLOW != 0
-    set_color brcyan
     set --global __fish_git_prompt_show_informative_status 1
+    set --global __fish_git_prompt_showcolorhints 1
     fish_git_prompt
   end
 
-  set_color normal
   printf '\f\r--> '
 end
