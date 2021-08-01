c966eee6fecf83d0b17597d70dbd3dc7d02f9cea
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-08-01 21:17:31 +0200

    [fish] Dynamically shorten path in prompt when too long

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,14 +1,36 @@
-function fish_prompt
-  echo -n (set_color magenta)$USER@(prompt_hostname)(set_color normal)
+function __is_prompt_too_long
+  set prompt $argv[1]
+  set prompt_length (string length $prompt)
+  return (test $prompt_length -gt $COLUMNS)
+end
 
+function fish_prompt
+  set user_section $USER@(prompt_hostname)
   set --global fish_prompt_pwd_dir_length 0  # Disable path shortening
-  echo -n (set_color yellow) (prompt_pwd)(set_color normal)
+  set pwd_full_section (prompt_pwd)
 
   if not set --query GIT_BASED_WORKFLOW ; or test $GIT_BASED_WORKFLOW != 0
     set --global __fish_git_prompt_show_informative_status 1
+    set --erase __fish_git_prompt_showcolorhints
+    set git_uncolored_section (fish_git_prompt)
     set --global __fish_git_prompt_showcolorhints 1
-    fish_git_prompt
+    set git_colored_section (fish_git_prompt)
   end
+
+  set should_shorten_path 1
+  set pwd_section $pwd_full_section
+  while test $should_shorten_path -ne 0
+    if __is_prompt_too_long (echo '[I]' $user_section $pwd_section $git_uncolored_section)
+      set pwd_full_section (string replace --regex '^\/?~?[^\/]+\/' '' $pwd_full_section)
+      set pwd_section /.../$pwd_full_section
+    else
+      set should_shorten_path 0
+    end
+  end
+
+  echo -n (set_color magenta)$user_section(set_color normal)
+  echo -n (set_color yellow) $pwd_section(set_color normal)
+  echo -n $git_colored_section
 
   printf '\f\r--> '
 end
