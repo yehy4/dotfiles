ae04edfdbac3627a52e1ecef9b323dffc4a3d2f6
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-09-21 16:50:01 +0200

    [neovim] Use ripgrep instead of grep

--- neovim/lua/settings/init.lua
+++ neovim/lua/settings/init.lua
@@ -52,7 +52,7 @@ vim.o.path = '**'
 vim.o.modeline = false
 
 -- Use ripgrep when searching using :grep or :lgrep
-vim.o.grepprg = 'grep --recursive --line-number'
+vim.o.grepprg = 'rg --vimgrep --smart-case'
 
 -- Disable timeout when waiting for a sequence of keystrokes
 vim.o.timeout = false
