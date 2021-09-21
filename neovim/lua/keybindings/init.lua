846b9e1f4b05748691c5a73502848d15da51942d
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-09-21 19:35:50 +0200

    [neovim] Update comments for keybindings

--- neovim/lua/keybindings/init.lua
+++ neovim/lua/keybindings/init.lua
@@ -7,37 +7,37 @@ local function map_keys(mode, keys, command)
   vim.api.nvim_set_keymap(mode, keys, command, {noremap = true})
 end
 
--- Use <leader>tf to fuzzy search
+-- Fuzzy search
 map_keys('n', '<leader>tg', ':Telescope live_grep<cr>')
 
--- Use <leader>tf to for fuzzy file name search
+-- Fuzzy search for files
 map_keys('n', '<leader>tf', ':Telescope find_file<cr>')
 
--- Use <leader>y to perform OSC52 yanking when in visual mode
+-- Yank using OSC52 when in visual mode
 map_keys('v', '<leader>y', ':OSCYank<cr>')
 
--- Use <leader>b to toggle directory browser tree
+-- Toggle directory browser tree
 map_keys('n', '<leader>b', ':NvimTreeToggle<cr>')
 
 -- Keybindings that are only available to the buffers attached to an LSP
 keybindings.activate_lsp_buffer_bindings = function(bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_map_keys(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 
   local opts = { noremap=true, silent=true }
-  buf_set_keymap('n', '<leader>lf', ':lua vim.lsp.buf.formatting()<CR>', opts)
-  buf_set_keymap('n', '<leader>lh', ':lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<leader>ls', ':lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<leader>lr', ':lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<leader>lt', ':lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>la', ':lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', '<leader>lg', ':lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<leader>li', ':lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<leader>ldc', ':lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', '<leader>ldf', ':lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>lD', ':lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+  buf_map_keys('n', '<leader>lf', ':lua vim.lsp.buf.formatting()<CR>', opts)
+  buf_map_keys('n', '<leader>lh', ':lua vim.lsp.buf.hover()<CR>', opts)
+  buf_map_keys('n', '<leader>ls', ':lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_map_keys('n', '<leader>lr', ':lua vim.lsp.buf.rename()<CR>', opts)
+  buf_map_keys('n', '<leader>lt', ':lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_map_keys('n', '<leader>la', ':lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_map_keys('n', '<leader>lg', ':lua vim.lsp.buf.references()<CR>', opts)
+  buf_map_keys('n', '<leader>li', ':lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_map_keys('n', '<leader>ldc', ':lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_map_keys('n', '<leader>ldf', ':lua vim.lsp.buf.definition()<CR>', opts)
+  buf_map_keys('n', '<leader>lD', ':lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
 
-  -- Use <leader>lc for omni completion
-  buf_set_keymap('i', '<leader>lc', '<C-x><C-o>', opts)
+  -- Omni complete
+  buf_map_keys('i', '<leader>lc', '<C-x><C-o>', opts)
 end
 
 return keybindings
