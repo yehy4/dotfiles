0710c26efda492a5d467fed5fbbeae70c5aa2359
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-09-21 13:49:08 +0200

    [neovim] Modify theme for Telescope commands

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -2,7 +2,8 @@ require('packer').use {
   'https://github.com/nvim-telescope/telescope.nvim',
   requires = {'https://github.com/nvim-lua/plenary.nvim'},
   config = function()
-    vim.api.nvim_set_keymap('n', '<leader>tf', '<cmd>Telescope find_files<cr>', {noremap = true})
-    vim.api.nvim_set_keymap('n', '<leader>tg', '<cmd>Telescope live_grep<cr>', {noremap = true})
+    local keymap = vim.api.nvim_set_keymap
+    keymap('n', '<leader>tf', '<cmd>Telescope find_files theme=ivy<cr>', {noremap = true})
+    keymap('n', '<leader>tg', '<cmd>Telescope live_grep theme=ivy<cr>', {noremap = true})
   end
 }
