f4ee679b041a3fe0291d7cc91417db792173e687
yehy4 <25834760+yehy4@users.noreply.github.com>
2021-12-07 23:19:06 +0100

    [neovim] Add plugin for indentation guidelines

--- neovim/lua/plugins/theme.lua
+++ neovim/lua/plugins/theme.lua
@@ -3,3 +3,10 @@ require('packer').use({
   'https://github.com/glepnir/zephyr-nvim',
   config = function() vim.cmd('colorscheme zephyr') end
 })
+
+-- Show indentation guides
+require('packer').use({
+  'https://github.com/lukas-reineke/indent-blankline.nvim',
+  config = function() require("indent_blankline").setup {} end
+})
+
