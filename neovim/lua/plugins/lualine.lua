133d92b166797ed1d384555b501acc687f4ca7a2
yehy4 <25834760+yehy4@users.noreply.github.com>
2022-04-24 23:45:41 +0200

    [neovim] Use global status line for all windows

--- neovim/lua/plugins/lualine.lua
+++ neovim/lua/plugins/lualine.lua
@@ -2,11 +2,6 @@ require('packer').use({
   'https://github.com/nvim-lualine/lualine.nvim',
   requires = {'https://github.com/kyazdani42/nvim-web-devicons'},
   config = function()
-    local filename_section_settings = {
-      'filename',
-      path = 1 -- Use relative filepath.
-    }
-
     local b_section = enable_git_features and {'branch', 'diff'} or {}
     table.insert(b_section, {'diagnostics', sources = {'nvim_diagnostic'}})
 
@@ -14,10 +9,20 @@ require('packer').use({
       options = {
         theme = 'onenord',
         section_separators = {left = '', right = ''},
-        component_separators = ''
+        component_separators = '',
+        -- Use one status line for all windows (equivalent to 'set laststatus=3')
+        globalstatus = true
       },
-      sections = {lualine_b = b_section, lualine_c = {filename_section_settings}},
-      inactive_sections = {lualine_c = {filename_section_settings}},
+      sections = {
+        lualine_b = b_section,
+        lualine_c = {
+          {
+            'filename',
+            path = 1 -- Use relative filepath.
+          }
+        }
+      },
+
       extensions = {'nvim-tree'}
     })
   end
