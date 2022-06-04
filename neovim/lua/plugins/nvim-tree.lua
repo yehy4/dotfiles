ef9e1a96e6a7df82d6edde5ef4ee089936b908bf
yehy4 <25834760+yehy4@users.noreply.github.com>
2022-06-04 10:04:47 +0200

    [neovim] Replace deprecated nvim-tree option

--- neovim/lua/plugins/nvim-tree.lua
+++ neovim/lua/plugins/nvim-tree.lua
@@ -2,9 +2,6 @@ require('packer').use({
   'https://github.com/kyazdani42/nvim-tree.lua',
   requires = {'https://github.com/kyazdani42/nvim-web-devicons'},
   config = function()
-    -- Show the current working directory name without path at the top of the tree.
-    vim.g.nvim_tree_root_folder_modifier = ':t'
-
     -- Refresh tree when focus is regained. This is useful, for example, if files are modified
     -- outside of Neovim.
     vim.api.nvim_create_autocmd('FocusGained', {command = ':NvimTreeRefresh'})
@@ -23,8 +20,12 @@ require('packer').use({
           window_picker = {enable = false}
         }
       },
-      -- Show indentation marks for open folders.
-      renderer = {indent_markers = {enable = true}}
+      renderer = {
+        -- Show the current working directory name without path at the top of the tree.
+        root_folder_modifier = ':t',
+        -- Show indentation marks for open folders.
+        indent_markers = {enable = true}
+      }
     })
   end
 })
