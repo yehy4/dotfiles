bf0d3c352946f064f48bfac8fdade7fbf0d4c535
yehy4 <25834760+yehy4@users.noreply.github.com>
2022-06-05 12:30:48 +0200

    [neovim] Fix OSC 52 yanking after tmux update

--- neovim/lua/plugins/vim-oscyank.lua
+++ neovim/lua/plugins/vim-oscyank.lua
@@ -1,1 +1,5 @@
-require('packer').use({'https://github.com/ojroques/vim-oscyank'})
+require('packer').use({
+  'https://github.com/ojroques/vim-oscyank',
+  -- This is required after tmux 3.3. See https://github.com/ojroques/vim-oscyank/issues/26.
+  config = function() vim.g.oscyank_term = 'default' end
+})
